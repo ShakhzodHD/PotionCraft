ad852da5b79dbe60abdf365ba1852e5634aa797c
ShahzodHD <khalimov202@mail.ru>
2024-08-12 15:48:38 +0200

    mini update
    realize opportunity to buy goblins for money
    remake cash table

--- Assets/Scripts/UI/RecruitGoblinSystem.cs
+++ Assets/Scripts/UI/RecruitGoblinSystem.cs
@@ -1,9 +1,24 @@
+using System.Diagnostics;
 using UnityEngine;
 
 public class RecruitGoblinSystem : MonoBehaviour
 {
+    [SerializeField] private int priceForWorkers;
+    [SerializeField] private int priceForSeller;
     public void GoblinBuy(GameObject gameObject)
     {
-        gameObject.SetActive(true);
+        if (gameObject.tag == "GoblinWorker" && CurrencyManager.instance.CanAfford(priceForWorkers))
+        {
+            gameObject.SetActive(true);
+            CurrencyManager.instance.SpendCurrency(priceForWorkers);
+        }
+        else
+        {
+            if (CurrencyManager.instance.CanAfford(priceForSeller))
+            {
+                gameObject.SetActive(true);
+                CurrencyManager.instance.SpendCurrency(priceForSeller);
+            }
+        }
     }
 }
